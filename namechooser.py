0abb089e1d74dd31dde6bbd2152fef030258e4a3
unknown <jzou00@stuy.edu>
2019-09-10 18:24:43 -0400

    got working version

--- namechooser.py
+++ namechooser.py
@@ -1,3 +1,5 @@
+import random 
+
 KREWES = {
     'orpheus': ['Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffany', 'Amanda', 'Junhee', 'Jackie',
 'Tyler', 'Emory', 'Ivan', 'Elizabeth', 'Pratham', 'Shaw', 'Eric', 'Yaru', 'Kelvin', 'Hong Wei', 'Michael',
@@ -7,4 +9,15 @@ KREWES = {
 'Hannah', 'Alex'],
     'endymion': ['Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason', 'Tammy', 'Albert', 'Kazi',
 'Derek', 'Brandon', 'Kenneth', 'Lauren', 'Biraj', 'Jeff', 'Jackson', 'Taejoon', 'Kevin', 'Jude', 'Sophie', 
-    
+'Kevin', 'Jude', 'Sophie', 'Henry', 'Coby', 'Manfred', 'Leia', 'Ahmed', 'Winston']
+}
+
+num = random.randint(0,3)
+if num == 0:
+    key = 'orpheus'
+elif num == 1:
+    key = 'rex'
+else:
+    key = 'endymion'
+
+print(random.choice(KREWES[key]))
