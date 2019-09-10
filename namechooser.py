b7ef582a5cd671c20fa9f112511070304bfa27ba
Jackson Zou <jzou00@stuy.edu>
2019-09-10 14:54:55 -0400

    Added names

--- namechooser.py
+++ namechooser.py
@@ -1,5 +1,10 @@
 KREWES = {
     'orpheus': ['Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffany', 'Amanda', 'Junhee', 'Jackie',
 'Tyler', 'Emory', 'Ivan', 'Elizabeth', 'Pratham', 'Shaw', 'Eric', 'Yaru', 'Kelvin', 'Hong Wei', 'Michael',
-'Kiran', 'Amanda', 'Joesph', 'Tanzim', 'David', 'Yevgeniy']
+'Kiran', 'Amanda', 'Joesph', 'Tanzim', 'David', 'Yevgeniy'],
+    'rex': ['William', 'Joesph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo', 'Peihua', 'Saad', 'Benjamin',
+'Justin', 'Alice', 'Hilary', 'Ayham', 'Michael', 'Matthew', 'Jionghao', 'Devin', 'David', 'Jacob', 'Will',
+'Hannah', 'Alex'],
+    'endymion': ['Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason', 'Tammy', 'Albert', 'Kazi',
+'Derek', 'Brandon', 'Kenneth', 'Lauren', 'Biraj', 'Jeff', 'Jackson', 'Taejoon', 'Kevin', 'Jude', 'Sophie', 
     
