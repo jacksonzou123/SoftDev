703618978e4ebfb047006bafbd226e4c76e53947
Jackson Zou <jzou00@stuy.edu>
2019-09-10 14:50:44 -0400

    created python file for name choosing exercise

---
+++ namechooser.py
@@ -0,0 +1,5 @@
+KREWES = {
+    'orpheus': ['Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffany', 'Amanda', 'Junhee', 'Jackie',
+'Tyler', 'Emory', 'Ivan', 'Elizabeth', 'Pratham', 'Shaw', 'Eric', 'Yaru', 'Kelvin', 'Hong Wei', 'Michael',
+'Kiran', 'Amanda', 'Joesph', 'Tanzim', 'David', 'Yevgeniy']
+    
