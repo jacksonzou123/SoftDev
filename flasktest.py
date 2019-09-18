e393daf1d883c65da791c63ee609923e736c1bc4
Jackson <jzou00@stuy.edu>
2019-09-18 14:50:33 -0400

    playing with flask

--- flasktest.py
+++ flasktest.py
@@ -1,7 +1,8 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__) #creates a instance of the class
 
 @app.route("/")
+@app.route("/2") #gives the route of the app, home directory
 def hello_world():
     print(__name__)
     return "No hablo queso!"
@@ -9,3 +10,4 @@ def hello_world():
 if __name__ == "__main__":
     app.debug = True
     app.run()
+
