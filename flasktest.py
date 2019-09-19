94fd2ab13f0f1e0c804cf8c00322c2ff6051b46f
Jackson Zou <jzou00@stuy.edu>
2019-09-18 21:01:58 -0400

    added 3 more routes

--- flasktest.py
+++ flasktest.py
@@ -1,11 +1,25 @@
 from flask import Flask
 app = Flask(__name__) #creates a instance of the class
 
-@app.route("/")
-@app.route("/2") #gives the route of the app, home directory
+@app.route("/") #gives the route of the app, home directory
 def hello_world():
     print(__name__)
     return "No hablo queso!"
+
+@app.route("/yo")
+def yo():
+    print(__name__)
+    return "Yo hablo queso!"
+
+@app.route("/bleh")
+def bleh():
+    print(__name__)
+    return "bleh"
+
+@app.route("/num")
+def num():
+    print(__name__)
+    return "num"
 
 if __name__ == "__main__":
     app.debug = True
