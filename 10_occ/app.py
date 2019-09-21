c4cf35f241b69c4f597469fba7b6eb4c1a275d86
Jackson <jzou00@stuy.edu>
2019-09-20 21:41:11 -0400

    added the stuff for 10_occ assignment

---
+++ 10_occ/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hi, this is the homepage"
+
+@app.route("/occupyflaskst")
+def template():
+    return render_template(
+        'app.html',
+        foo="fooooo",
+        collection=coll
+    )
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
