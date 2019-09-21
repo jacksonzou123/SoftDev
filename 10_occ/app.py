2b015c09067a8616032749762455d683a6166dea
Jackson <jzou00@stuy.edu>
2019-09-20 22:10:10 -0400

    able to print out random job

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -1,16 +1,42 @@
 from flask import Flask, render_template
+import random
 app = Flask(__name__)
+
+def get_job():
+    file = open("data/occupations.csv", 'r')
+    words = file.readlines()
+    words.pop(0)
+    words.pop() #remove first and last because they aren't part of the list
+    index = 0
+    rolls = {}
+    for each in words:
+        each = each.strip()
+        look = len(each)-1
+        job = 0
+        percent = 0
+        while look >= 0:
+            if each[look] == ',': #look for the last comma in each line
+                job = each[0:look]
+                percent = float(each[look+1:])
+                look = -1
+            look -= 1
+        while percent > 0:
+            index += 1
+            rolls[index] = job #scale everything by 10, and give each percentage point a job
+            percent -= 0.1
+    roll = random.randint(1,998) #print job
+    return rolls[roll]
 
 @app.route("/")
 def home():
     return "Hi, this is the homepage"
 
 @app.route("/occupyflaskst")
-def getjob():
+def showjob():
     return render_template(
         'app.html',
-        foo="fooooo",
-        collection=coll
+        foo="Random Job Selection",
+        item=get_job()
     )
 
 if __name__ == "__main__":
