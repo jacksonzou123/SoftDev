65e7bda9733c2fc003b844bf39070c4cd759fb2f
Jackson <jzou00@stuy.edu>
2019-09-25 15:04:15 -0400

    got some input working

---
+++ new/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('foo.html')
+
+@app.route("/auth")
+def maybe():
+    print(request.form)
+    print(request)
+    print(request.args)
+    print(request.headers)
+    print(request.method)
+    print(app)
+    return "jacb sucks"
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
