2d27d5a9c0403d3a35d916e40fb31e12f29556fd
Jackson <jzou00@stuy.edu>
2019-09-26 14:30:48 -0400

    Added print statements

--- 12_form/app.py
+++ 12_form/app.py
@@ -7,6 +7,9 @@ def home():
 
 @app.route("/auth")
 def maybe():
+    #print (request.args)
+    #print (request.method)
+    #print (app)
     return render_template('poo.html',
                             name = request.args['name'],
                             args = request.method)
