60ca9b558ab37b2bbfb81a60db2e31137dd45ff5
Jackson <jzou00@stuy.edu>
2019-09-26 14:46:21 -0400

    added print statements

--- 12_form/app.py
+++ 12_form/app.py
@@ -1,3 +1,8 @@
+#The HillBillies: Grace Mao, Jackson Zou
+#SoftDev1 pd9
+#K12 -- Echo echo echo
+# 2019-10-02
+
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
@@ -7,9 +12,17 @@ def home():
 
 @app.route("/auth")
 def maybe():
-    #print (request.args)
-    #print (request.method)
-    #print (app)
+    # print("\n\n\n")
+    # print("***DIAG: this Flask obj ***")
+    # print(app)
+    # print("***DIAG: request obj ***")
+    # print(request)
+    # print("***DIAG: request.args ***")
+    # print(request.args)
+    # print("***DIAG: request.args['username'] ***")
+    # print(request.args['name'])
+    # print("***DIAG: request.headers ***")
+    # print(request.headers)
     return render_template('poo.html',
                             name = request.args['name'],
                             args = request.method)
