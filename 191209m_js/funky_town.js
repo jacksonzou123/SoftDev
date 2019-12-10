17edf3ba330d270ead4e59d9a95a552484f5f75b
Jackson Zou <jzou00@stuy.edu>
2019-12-10 14:55:38 -0500

    added 27 something

---
+++ 191209m_js/funky_town.js
@@ -0,0 +1,41 @@
+var foo = function() {
+  console.log("diagnostic info here");
+  return 0;
+};
+
+var fact = function(n) {
+  if (n < 2) {
+    return 1;
+  }
+  else {
+    return n * fact(n-1);
+  }
+};
+
+var fibonacci = function(n) {
+  if (n <= 2) {
+    return 1;
+  }
+  else {
+    return fibonacci(n-1) + fibonacci(n-2);
+  }
+}
+
+var gcd = function(a, b) {
+  if (a < b) {
+    i = a;
+  }
+  else {
+    i = b;
+  }
+  while (i > 1) {
+    if (a % i == 0 && b % i == 0) {
+      return i;
+    }
+    i--;
+  }
+  return 1;
+}
+
+var randomStudent = function() {
+}
